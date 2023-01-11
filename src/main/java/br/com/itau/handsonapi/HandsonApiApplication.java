6d8defb22bd8aaf670d2b37a8cb2fd70a662dda2
Wesley Freire <wesley.freire@live.com>
2023-01-11 00:50:30 -0300

    Versão Inicial HandsOn API

---
+++ src/main/java/br/com/itau/handsonapi/HandsonApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.itau.handsonapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HandsonApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HandsonApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/itau/handsonapi/HandsonApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.itau.handsonapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HandsonApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
